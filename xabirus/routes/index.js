ec933cf0c7bdc40b6c7f4d9027240e1038b33077
Xabin Rodriguez <xabin95@gmail.com>
2018-03-22 11:11:14 +0100

    Removed dependencies, added bootstrap

--- xabirus/routes/index.js
+++ xabirus/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Papa's House" });
+  res.render("index", { title: "Xabirus" });
 });
 
 module.exports = router;
